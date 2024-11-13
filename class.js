57e6969b966cc5539b176db7df89798dba704fde
jannyyyy1 <jannyzzz@naver.com>
2024-11-13 21:13:16 +0900

    중력 힘 적용

--- class.js
+++ class.js
@@ -1,4 +1,4 @@
-class mover {
+class Mover {
     constructor(x, y) {
       this.position = createVector(x, y); // 위치 벡터
       this.velocity = createVector(2, 2); // 속도 벡터
--- sketch.js
+++ sketch.js
@@ -2,9 +2,12 @@ let mover;
 
 function setup() {
   createCanvas(400, 400);
-  mover = new mover(width / 2, height / 2); // 화면 중간에 물체 생성
+  mover = new Mover(width / 2, height / 2); // 화면 중간에 물체 생성
 }
 
 function draw() {
   background(220);
+
+  let gravity = createVector(0, 0.1); // 중력 벡터
+  mover.applyForce(gravity); // 중력 힘 적용
 }
