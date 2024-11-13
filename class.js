c3ce1e079bc68a574e30cc815786463470ceab67
jannyyyy1 <jannyzzz@naver.com>
2024-11-13 21:08:49 +0900

    질량과 가속도 벡터 추가, 질량에 따른 힘 적용

--- class.js
+++ class.js
@@ -1,10 +1,15 @@
-class Mover {
+class mover {
     constructor(x, y) {
       this.position = createVector(x, y); // 위치 벡터
       this.velocity = createVector(2, 2); // 속도 벡터
+      this.acceleration = createVector(0, 0); // 가속도 벡터 초기화
+      this.mass = 1; // 물체의 질량
     }
 
     applyForce(force) {
         this.velocity.add(force); // 힘을 속도에 더함
+        let f = force.copy(); // 힘을 복사
+        f.div(this.mass); // 질량으로 나누어 가속도 계산
+        this.acceleration.add(f); // 가속도에 추가
+        }
       }
-}
--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ let mover;
 
 function setup() {
   createCanvas(400, 400);
-  mover = new Mover(width / 2, height / 2); // 화면 중간에 물체 생성
+  mover = new mover(width / 2, height / 2); // 화면 중간에 물체 생성
 }
 
 function draw() {
