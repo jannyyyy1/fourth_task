5d000ea40a49fb062550ce7a38416fd27bc10a36
jannyyyy1 <jannyzzz@naver.com>
2024-11-13 21:00:30 +0900

    힘 적용

--- class.js
+++ class.js
@@ -3,4 +3,8 @@ class Mover {
       this.position = createVector(x, y); // 위치 벡터
       this.velocity = createVector(2, 2); // 속도 벡터
     }
+
+    applyForce(force) {
+        this.velocity.add(force); // 힘을 속도에 더함
+      }
 }
